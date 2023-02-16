a7e8776d75296153f929aef5d399372d373ed766
PavelZubarev <213633@inbox.ru>
2023-02-16 20:38:50 +0300

    'down'

---
+++ src/Service/TelegramBotClient.php
@@ -0,0 +1,8 @@
+<?php
+namespace Delorkor\TelegramBotClient\Service;
+class TelegramBotClient
+{
+    private const API = 'https://api.telegram.org/bot';
+    private const SEND_MESSAGE = 'sendMessage';
+    private const GET_UPDATES = 'getUpdates';
+}
